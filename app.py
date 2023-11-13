04eee576421c4942507a07ffc160b4d7e224fcb3
Sandra Fernandes <sandypina07@gmail.com>
2023-11-13 09:51:15 -0100

    Efetuado carga de dados e apresentado dataframe

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
 import pandas as pd 
 import numpy as np
+import streamlit as st
+
+def load_data():
+    return pd.read_csv('data/processed/bikes_completed.csv')
+
+def = load_data()
+
+st.dataframe(df)
+
+
+
+
+
+
